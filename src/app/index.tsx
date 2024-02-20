bb6e696c43d33f97b932f66dc6995294b1c8229c
Neander de Souza <neander.faria@hotmail.com>
2024-02-20 10:58:05 -0300

    Aula 03

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,13 +8,13 @@ import { Product } from '@/components/product';
 
 import { useCartStore } from '@/stores/cart-store';
 
-import { CATEGORIES, MENU } from '@/utils/data/products';
+import { CATEGORIES, MENU, ProductProps } from '@/utils/data/products';
 
 export default function Home() {
     const cartStore = useCartStore();
     const [category, setCategory] = useState(CATEGORIES[0]);
 
-    const sectionListRef = useRef<SectionList>(null);
+    const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
     const cartQuantityItems = cartStore.products.reduce(
         (total, product) => total + product.quantity,
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 import { ProductProps } from "@/utils/data/products";
 
@@ -11,13 +13,30 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[];
   add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
+  clear: () => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
 
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
-}));
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+
+      clear: () => set({ products: [] }),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
